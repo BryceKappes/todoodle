0da42864227b750bc1e8f810b00cbb71260bb130
Bryce Kappes <brycebkappes@gmail.com>
2021-04-16 14:44:10 -0500

    Added a youtube video to carousel

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,16 +1,15 @@
 import { Carousel } from 'react-bootstrap';
 import Image from '../assets/images/example.jpg'
+import { MDBContainer, MDBIframe } from "mdbreact";
 
 function Youtube(){
   return (
     <div className="carousel">
     <Carousel>
         <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src= { Image }
-            alt="First slide"
-            />
+            <MDBContainer className="text-center">
+                <MDBIframe src="https://www.youtube.com/embed/v64KOxKVLVg" />
+            </MDBContainer>
             <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
